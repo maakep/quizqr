9aaf1adf2cd9631271f62f96459fc82cf795a513
Maakep <schon.fredrik@gmail.com>
2019-09-29 01:36:01 +0200

    Add mock quiz and make it scan for questions n stuff

--- src/Quiz/main.tsx
+++ src/Quiz/main.tsx
@@ -6,56 +6,119 @@ import { DeviceAndScanner } from '../../types';
 
 
 const TEAM_NAME = 'TEAM_NAME';
+const QUIZ = 'QUIZ_NAME';
 
-export default (): JSX.Element => {
-  const [input, setInput] = React.useState<string>('');
-  const [error, setError] = React.useState<string>('');
-  const [team, setTeam] = React.useState<string>(localStorage.getItem(TEAM_NAME) || '');
+interface IQuestion {
+  question: string;
+  answers: string[];
+  correctAnswer: number;
+};
 
-  const submit = () => {
-    if (input.length > 3) {
-      localStorage.setItem(TEAM_NAME, input);
-      setTeam(input);
-    }
-    else {
-      setError('Minimum 4 characters');
-    }
+export interface IQuiz {
+  name: string;
+  questions: IQuestion[];
+};
+
+interface IProps {
+
+};
+
+interface IState {
+  error: string;
+  team: string;
+  teamInput: string;
+  quiz: IQuiz;
+  quizInput: string;
+}
+
+export default class extends React.PureComponent<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+
+    const quiz = JSON.parse(localStorage.getItem(QUIZ)) || undefined;
+    const team = localStorage.getItem(TEAM_NAME) || '';
+
+    this.state = {
+      error: '',
+      team: team,
+      teamInput: '',
+      quiz: quiz,
+      quizInput: '',
+    };
   }
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(e.currentTarget.value);
-    setError('');
+  inputWithButton = (placeholder: string, submit: () => void, onChange: (text: string) => void) => {
+    return (
+      <div style={style.center}>
+        <input
+          key={placeholder}
+          autoFocus
+          style={style.input}
+          type="text"
+          onChange={(e) => onChange(e.currentTarget.value)}
+          placeholder={placeholder}
+        />
+        <button style={style.button} onClick={() => submit()}>Go</button>
+      </div>
+    );
   }
 
-  const noTeamSelected = () => {
+  do(ms: number, func: () => void): void {
+    setTimeout(func, ms)
+  }
+
+  selectTeam = () => {
+    const submit = () => {
+      const input = this.state.teamInput;
+      if (input.length > 3) {
+        localStorage.setItem(TEAM_NAME, input);
+        this.setState({ team: input });
+      }
+      else {
+        this.setState({ error: 'Minimum 4 characters' });
+      }
+    }
+
     return (
       <>
-        <div style={style.center}>
-          <input
-            autoFocus
-            style={style.input}
-            type="text"
-            onChange={handleChange}
-            placeholder={"Enter your team name"}
-          />
-          <button style={style.button} onClick={() => submit()}>Go</button>
-        </div>
+        {this.inputWithButton("Enter your team name", submit, (input) => this.setState({ teamInput: input }))}
         <div style={styles(style.center, style.error, style.text)}>
-          {error}
+          {this.state.error}
         </div>
       </>
     );
   };
 
-  if (team === '') {
-    return noTeamSelected();
-  } else {
-    return (
-      <Quiz team={team} />
-    )
+  selectQuiz = () => {
+
+    const submit = async () => {
+      // const res = await fetch(`/Quiz/${this.state.quizInput}`);
+      // const json: IQuiz = await res.json();
+      // if (json.success)...
+      const mockedJson: IQuiz = {
+        name: 'test quiz',
+        questions: [{
+          question: 'What is 1+1?',
+          answers: ['Pizza'],
+          correctAnswer: 0,
+        },
+        {
+          question: 'What is the best food?',
+          answers: ['Taco', 'Pizza', '3'],
+          correctAnswer: 1,
+        }],
+      }
+      localStorage.setItem(QUIZ, JSON.stringify(mockedJson));
+      this.setState({ quiz: mockedJson });
+    }
+
+    return this.inputWithButton("Enter Quiz name", submit, (input) => this.setState({ quizInput: input }));
   }
+  render() {
+    if (this.state.team === '') return this.selectTeam();
+    else if (!this.state.quiz) return this.selectQuiz();
+    else return <Quiz team={this.state.team} quiz={this.state.quiz} />;
+
+  }
+
 }
-
-
-
-
--- src/Quiz/quiz.tsx
+++ src/Quiz/quiz.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { BrowserQRCodeReader } from '@zxing/library';
+import { IQuiz } from './main';
 
 
-type Props = { team: string };
+type Props = { team: string, quiz: IQuiz };
 export default class Bla extends React.PureComponent<Props, { text: string }> {
   scanner: BrowserQRCodeReader;
 
@@ -15,7 +16,7 @@ export default class Bla extends React.PureComponent<Props, { text: string }> {
 
   decode = () => {
     this.scanner.decodeOnceFromVideoDevice(undefined, 'video').then((result) => {
-      this.setState({ text: result.getText() });
+      this.setState({ text: this.props.quiz.questions[parseInt(result.getText())].question });
     }).catch((err: string) => {
       console.warn(err);
     }).finally(() => {
@@ -30,8 +31,9 @@ export default class Bla extends React.PureComponent<Props, { text: string }> {
           {this.state.text}
         </p>
         <video
-          onMouseDown={() => this.decode()}
-          onMouseUp={() => {
+          onTouchStart={() => this.decode()}
+          onTouchEnd={() => {
+            this.scanner.reset();
           }}
           id="video" width="100%" height="80%" autoPlay={false} style={{ border: '1px solid black' }}></video>
       </>
